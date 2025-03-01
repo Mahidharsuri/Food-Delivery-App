50f29912f8d67cb56575c52755a3b7f547045cab
Mahidharsuri <mahidharsuri07@gmail.com>
2025-03-01 13:07:43 +0530

    first commit

---
+++ my-proj/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order Your favorite food here</h2>
+            <p>choose from a diverse menu featuring delectable array of dishes crafted with finest ingridients and culinary expertise</p>
+            <button>View Menu</button>
+        </div>
+         
+    </div>
+  )
+}
+
+export default Header
